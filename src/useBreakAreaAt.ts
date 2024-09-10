742dbf02b69c0d0b7ab56e553bc3ec6f68f02cfc
Songhyeon Jun <shjeon0730@gmail.com>
2024-09-09 23:40:27 -0500

    fix wrong render error

--- src/useBreakAreaAt.ts
+++ src/useBreakAreaAt.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { UPDATE_BREAK_AREA } from './consts';
 import { BreakAreaContext } from './BreakAreaProvider';
 import { getMsgOwnerId } from './utils';
@@ -7,16 +7,22 @@ export const useBreakAreaAt = (id: string, breakArea: string) => {
 	const { dataRef } = useContext(BreakAreaContext);
 	const [isInBoundary, setInBoundary] = useState(dataRef.current?.[id]?.current === breakArea);
 	const msgId = useMemo(() => getMsgOwnerId(id), [id]);
+	const setIsBoundaryRef = useRef(isInBoundary);
+	setIsBoundaryRef.current = isInBoundary;
 	useEffect(() => {
 		const listener: EventListenerOrEventListenerObject = e => {
 			const ev = e as CustomEvent;
-			if (ev.type === UPDATE_BREAK_AREA) {
-				if (ev.detail.id === msgId) {
-					setInBoundary(ev.detail.current === breakArea);
+			if (ev.type === UPDATE_BREAK_AREA && ev.detail.id === msgId) {
+				const newVal = ev.detail.current === breakArea;
+				if (setIsBoundaryRef.current !== newVal) {
+					setTimeout(() => setInBoundary(newVal)); // use event queue to remove race condition.
 				}
 			}
 		};
 		window.addEventListener(UPDATE_BREAK_AREA, listener);
+		return () => {
+			window.removeEventListener(UPDATE_BREAK_AREA, listener);
+		};
 	}, [msgId]);
 
 	return isInBoundary;
